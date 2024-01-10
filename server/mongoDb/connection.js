1d0091a58cf00c27fd91873ff40e4195bb1042d5
priyanjan <priyanjanjb@gmail.com>
2024-01-10 13:31:45 +0530

    connect mongodb and setup connection

--- server/mongoDb/connection.js
+++ server/mongoDb/connection.js
@@ -4,11 +4,11 @@ async function main() {
   const uri =
     "mongodb+srv://priyanjanjb:v5eueYJfiBlVvU12@carelabel.gdkbswt.mongodb.net/";
 
-  const carelabel = new MongoClient(uri);
+  const carelabel = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
     await carelabel.connect();
-    console.log("connected to database");
+    console.log("connected to the database");
     const list = await listDatabases(carelabel);
     console.log(list);
   } catch (e) {
@@ -17,4 +17,17 @@ async function main() {
     await carelabel.close();
     console.log("connection closed");
   }
+}
+
+main().catch(console.error);
+
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
 }
